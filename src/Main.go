11c5be1a1596bf70ba1be0134235eccea79ebcec
Derpitron <priya.aravamuthan@gmail.com>
2021-07-30 17:20:22 +0530

    YEAAAAAA It works

--- src/Main.go
+++ src/Main.go
@@ -22,7 +22,6 @@ func goDotEnvVariable(key string) string {
 }
 
 var logger = logrus.New()
-
 var (
 	hook, _ = disgohook.NewWebhookClientByToken(nil, logger, goDotEnvVariable("HOOK"))
 	placeId string
@@ -31,51 +30,39 @@ var (
 
 func GetProcessByName(targetProcessName string) *process.Process {
 	processes, _ := process.Processes()
-
 	for _, proc := range processes {
 		name, _ := proc.Name()
-
 		if name == targetProcessName {
 			return proc
 		}
 	}
-
 	return nil
 }
 
 func UpdateRobloxPresence() {
 	roblox := GetProcessByName("RobloxPlayerBeta.exe")
-
 	for roblox == nil {
 		roblox = GetProcessByName("RobloxPlayerBeta.exe")
-
 		if !reset {
 			reset = true
-
 			fmt.Println("reset client activity")
 		}
 	}
 
 	reset = false
-
 	args, _ := roblox.Cmdline()
 
 	placePattern := regexp.MustCompile(`placeId=(\d+)`)
 	placeMatch := placePattern.FindStringSubmatch(args)[1]
-
 	if placeMatch != placeId {
 		placeId = placeMatch
-
 		_, _ = hook.SendContent("`Started playing:`\nhttps://www.roblox.com/games/" + placeId + "/-")
-
-		fmt.Println("set activity: " + placeId)
 	}
 }
 
 func main() {
 	for {
 		UpdateRobloxPresence()
-
-		time.Sleep(time.Second * 5)
+		time.Sleep(time.Second * 3)
 	}
 }
